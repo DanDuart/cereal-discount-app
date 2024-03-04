ae463024d8162f6256e83e99b8d75fcf28cc6ab0
Victor Feijo <victor.feijoa@gmail.com>
2024-03-04 17:25:00 -0300

    Feedbacks from challenge

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,3 +1,6 @@
+// CON: Logic is concentraded in a single function which adds a lot to the complexity,
+// besieds that, it's hard to understand the logic of the function, and it's hard to test.
+
 module.exports = {
   calculateDiscountedPrice: (discountConfig, cartData) => {
     const { lineItems } = cartData.cart;
@@ -13,11 +16,16 @@ module.exports = {
       const { sku, price } = item;
       const itemPrice = parseFloat(price);
 
+      // PRO: Checking the correct logic for the discount
       if (isEligibleForDiscount && lineItems.length > 1 && !discountApplied) {
+        // CON: We're re-iterating the lineItems to find the eligible items
+        // on every iteration of the lineItems, which is unnecessary.
+        // Should move this function to ouside the forEach.
         const eligibleItems = lineItems.filter((el) =>
           discountConfig.eligible_skus.includes(el.sku)
         );
 
+        // CON: Same as the comment above, could be moved to outside the forEach.
         const cheapestEligibleItem =
           eligibleItems.length > 0 &&
           eligibleItems.reduce((minItem, currentItem) =>
